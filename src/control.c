d14ea4650d894e25e6f97d7029a72373cdb91723
Forrest Baer <gfbaer@gmail.com>
2019-11-27 19:43:45 -0700

    getting ready for nested logic, setting up helper functions and error handling

--- src/control.c
+++ src/control.c
@@ -16,6 +16,8 @@
 #include "interface.h"
 #include "engine.h"
 
+#define GATE_OUTS 8
+
 #define SPEEDCYCLE 4
 #define CLOCKOUTWIDTH 10
 
@@ -26,13 +28,16 @@
 #define SPEEDTIMER 0
 #define CLOCKTIMER 1
 #define CLOCKOUTTIMER 2
-#define GATETIMER 3
+#define ERRORTIMER 3
+#define GATETIMER 4
 
 preset_data_t p;
 preset_meta_t m;
 shared_data_t s;
 
 u8 selected_row;
+u8 do_blink_error;
+u8 do_blink_error_row;
 u8 current_tick;
 u8 is_preset_saved;
 u32 speed, speed_mod, gate_length_mod;
@@ -40,7 +45,7 @@ u32 speed, speed_mod, gate_length_mod;
 u8 clock_divs[12] = {128,64,32,16,8,7,6,5,4,3,2,1};
 
 static void set_trigger_bits(u8 r, u8 d);
-static int get_division(u8 pos);
+static u8 get_division(u8 p);
 
 static void save_preset(void);
 static void save_preset_with_confirmation(void);
@@ -48,8 +53,12 @@ static void save_preset_with_confirmation(void);
 static void update_speed_from_knob(void);
 static void update_speed(void);
 
+static u8 get_total_logical_refs(void);
+static u8 is_logically_referenced(u8 r);
+
 static void rotate_clocks(void);
-static void fire_gate(u8 row);
+static void fire_gate(u8 r);
+static void blink_error(u8 r);
 
 static void process_grid_press(u8 x, u8 y, u8 on);
 
@@ -71,7 +80,7 @@ void init_presets(void) {
     // initialize preset with default values 
     // store them to flash
     
-    for (u8 i = 0; i < 8; i++) {
+    for (u8 i = 0; i < GATE_OUTS; i++) {
         p.row[i].position = 15 - i;
         p.row[i].division = get_division(p.row[i].position);
         set_trigger_bits(i, p.row[i].division);
@@ -148,6 +157,8 @@ void process_event(u8 event, u8 *data, u8 length) {
                 step();
             } else if (data[0] == CLOCKOUTTIMER) {
                 set_clock_output(0);
+            } else if (data[0] == ERRORTIMER) {
+                do_blink_error = 1;             
             } else if (data[0] >= GATETIMER) {
                 set_gate(data[0] - GATETIMER, 0);
             }
@@ -190,25 +201,25 @@ void set_trigger_bits(u8 r, u8 d) {
         switch(p.row[r].logic.type) {
             case 1: // AND
                 for (int i = 0; i < MAX_STEPS; i++) {
-                    p.row[r].triggers[i] = p.row[r].triggers[i] && p.row[p.row[r].logic.compared_to_row].triggers[i] ? 1 : 0;
+                    p.row[r].triggers[i] = p.row[r].triggers[i] && p.row[p.row[r].logic.compared_to_row - 1].triggers[i] ? 1 : 0;
                 }
             break;
             case 2: // OR
                 for (int i = 0; i < MAX_STEPS; i++) {
-                    p.row[r].triggers[i] = p.row[r].triggers[i] || p.row[p.row[r].logic.compared_to_row].triggers[i] ? 1 : 0;
+                    p.row[r].triggers[i] = p.row[r].triggers[i] || p.row[p.row[r].logic.compared_to_row - 1].triggers[i] ? 1 : 0;
                 }
             break;
             case 3: // XOR
                 for (int i = 0; i < MAX_STEPS; i++) {
-                    p.row[r].triggers[i] = p.row[r].triggers[i] != p.row[p.row[r].logic.compared_to_row].triggers[i] ? 1 : 0;
+                    p.row[r].triggers[i] = p.row[r].triggers[i] != p.row[p.row[r].logic.compared_to_row - 1].triggers[i] ? 1 : 0;
                 }
             break;
         }
     }
 }
 
-int get_division(u8 pos) {
-    return clock_divs[pos - 4];
+u8 get_division(u8 p) {
+    return clock_divs[p - 4];
 }
 
 void save_preset_with_confirmation() {
@@ -232,7 +243,7 @@ void step() {
 
 void clock() {
     current_tick = (current_tick + 1) % MAX_STEPS;
-    for (u8 i = 0; i < 8; i++) {
+    for (u8 i = 0; i < GATE_OUTS; i++) {
         if (p.row[i].triggers[current_tick]) fire_gate(i);
     }
 }
@@ -242,8 +253,8 @@ void rotate_clocks() {
     u8 first_pos = p.row[0].position;
     u8 first_div = get_division(first_pos);
 
-    for (u8 i = 0; i < 8; i++) {
-        next_position = (next_position + 1) % 8;
+    for (u8 i = 0; i < GATE_OUTS; i++) {
+        next_position = (next_position + 1) % GATE_OUTS;
         if (next_position == 0) {
             p.row[7].position = first_pos;
             p.row[7].division = first_div;
@@ -256,11 +267,11 @@ void rotate_clocks() {
     }
 }
 
-void fire_gate(u8 row) {
+void fire_gate(u8 r) {
     // can set dynamic gate length here
-    add_timed_event(GATETIMER + row, 10, 0);
-    set_gate(row, 1);
-    p.row[row].blink = 1;
+    add_timed_event(GATETIMER + r, 10, 0);
+    set_gate(r, 1);
+    p.row[r].blink = 1;
 }
 
 void update_speed_from_knob() {
@@ -280,6 +291,22 @@ void update_speed() {
 void output_clock() {
     add_timed_event(CLOCKOUTTIMER, CLOCKOUTWIDTH, 0);
     set_clock_output(1);
+}
+
+u8 get_total_logical_refs() {
+    u8 total_logical_references = 0;
+    for (u8 i = 0; i < GATE_OUTS; i++) {
+        if (p.row[i].logic.compared_to_row > 0) total_logical_references++;
+    }
+    return total_logical_references;
+};
+
+u8 is_logically_referenced(u8 r) {
+    u8 is_referenced = 0;
+    for (u8 i = 0; i < GATE_OUTS; i++) {
+        if (p.row[i].logic.compared_to_row - 1 == r) is_referenced = 1;
+    }
+    return is_referenced;
 }
 
 void process_grid_press(u8 x, u8 y, u8 on) {
@@ -307,10 +334,18 @@ void process_grid_press(u8 x, u8 y, u8 on) {
     // logic buttons
     // sets logic type and comparison row
     //
-    if ((x > 0 && x < 4) && selected_row != y) {
+    if (x > 0 && x < 4) {
+        
+        // don't allow selection of logic on selected row (self referencing)
+        // don't allow all channels to have logic, otherwise a loop must exist
+        // don't allow a channel to be selected for logic if it's already selected for logic (logic loops)
+        if (selected_row == y || get_total_logical_refs() >= GATE_OUTS || is_logically_referenced(y)) {
+            blink_error(y);
+            return;
+        }
 
-        p.row[selected_row].logic.type = p.row[selected_row].logic.type == x && p.row[selected_row].logic.compared_to_row == y ? 0 : x;
-        p.row[selected_row].logic.compared_to_row = y;
+        p.row[selected_row].logic.type = p.row[selected_row].logic.type == x && p.row[selected_row].logic.compared_to_row - 1 == y ? 0 : x;
+        p.row[selected_row].logic.compared_to_row = y + 1;
         p.row[selected_row].division = get_division(p.row[selected_row].position);
 
         // reverse linked list for setting bits ?
@@ -318,23 +353,32 @@ void process_grid_press(u8 x, u8 y, u8 on) {
     }
 }
 
+void blink_error(u8 r) {
+    do_blink_error_row = r;
+    add_timed_event(ERRORTIMER, 333, 3);
+}
+
 void render_grid(void) {
     if (!is_grid_connected()) return;
 
+     // show that preset was saved with some flashing
     if (is_preset_saved) {
-        // show that preset was saved with some flashing
-        for (u8 i = 0; i < 8; i++) {
+        for (u8 i = 0; i < GATE_OUTS; i++) {
             set_grid_led(p.row[i].position, i, B_DIM);
         }
         is_preset_saved = 0;
     } else {
         clear_all_grid_leds();
 
-        for (u8 i = 0; i < 8; i++) {
+        if (do_blink_error) {
+            set_grid_led(1, do_blink_error_row, B_FULL);
+        }
+
+        for (u8 i = 0; i < GATE_OUTS; i++) {
             set_grid_led(0, selected_row, B_HALF);
-            set_grid_led(1, i, p.row[selected_row].logic.type == 1 && p.row[selected_row].logic.compared_to_row == i ? B_HALF : B_DIM);
-            set_grid_led(2, i, p.row[selected_row].logic.type == 2 && p.row[selected_row].logic.compared_to_row == i ? B_HALF : B_DIM);
-            set_grid_led(3, i, p.row[selected_row].logic.type == 3 && p.row[selected_row].logic.compared_to_row == i ? B_HALF : B_DIM);
+            set_grid_led(1, i, p.row[selected_row].logic.type == 1 && p.row[selected_row].logic.compared_to_row - 1 == i ? B_HALF : B_DIM);
+            set_grid_led(2, i, p.row[selected_row].logic.type == 2 && p.row[selected_row].logic.compared_to_row - 1 == i ? B_HALF : B_DIM);
+            set_grid_led(3, i, p.row[selected_row].logic.type == 3 && p.row[selected_row].logic.compared_to_row - 1 == i ? B_HALF : B_DIM);
             set_grid_led(p.row[i].position, i, p.row[i].blink ? 10 : B_HALF);
             p.row[i].blink = 0;
         }
