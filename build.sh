cc6de7e726e746d0c63b323813614d960f8a0121
Forrest Baer <gfbaer@gmail.com>
2019-12-03 08:26:37 -0700

    removing these

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd multipass/monome_euro/meadowphysics
-make clean
-make
-cp multipass_mp.hex ../../../dist/chrono-sage.hex
-make clean
-cd ../../../
-zip -r -X chrono-sage.zip dist
--- dist/flash.sh
+++
@@ -1,3 +0,0 @@
-dfu-programmer at32uc3b0256 erase
-dfu-programmer at32uc3b0256 flash chrono-sage.hex --suppress-bootloader-mem
-dfu-programmer at32uc3b0256 start
